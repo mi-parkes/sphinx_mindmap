a5e4cab51bc236cd9ebbc447f3d7c1e0cac8691c
Michael Parkes <mparkes@post.cz>
2024-07-22 20:02:10 +0200

    Formatted with black

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,25 +1,21 @@
 from git import Repo
 
-project     = 'sphinx_mindmap'
-copyright   = '2024, MP'
-author      = 'MP'
-version     = '0.5'
-release     = '0.5.0'
+project = "sphinx_mindmap"
+copyright = "2024, MP"
+author = "MP"
+version = "0.5"
+release = "0.5.0"
 
-extensions = [
-    'sphinx_mindmap',
-    'sphinx.ext.githubpages',
-    'myst_parser'
-]
+extensions = ["sphinx_mindmap", "sphinx.ext.githubpages", "myst_parser"]
 
-html_static_path = ['_static']
-templates_path   = ['_templates']
+html_static_path = ["_static"]
+templates_path = ["_templates"]
 exclude_patterns = []
 
-language = 'en'
+language = "en"
 
-html_theme      = 'sphinx_book_theme'
-html_css_files  = ['css/custom.css']
+html_theme = "sphinx_book_theme"
+html_css_files = ["css/custom.css"]
 
 repo = Repo("../..")
 current_branch = repo.active_branch
@@ -30,10 +26,10 @@ html_theme_options = {
     "repository_url": "https://github.com/mi-parkes/sphinx_mindmap",
     "repository_branch": branch_name,
     "show_navbar_depth": 2,
-    "show_toc_level": 1,  
+    "show_toc_level": 1,
     "use_repository_button": True,
     "use_source_button": True,
-    "home_page_in_toc" : True,
+    "home_page_in_toc": True,
     "use_issues_button": True,
-    "use_edit_page_button": True, 
+    "use_edit_page_button": True,
 }
