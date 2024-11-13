4bc03d6a0afbd41fbf214d920d77f3f35f578e85
Michael Parkes <mparkes@post.cz>
2024-11-13 17:00:09 +0100

    mc

--- sphinx_mindmap/__init__.py
+++ sphinx_mindmap/__init__.py
@@ -10,7 +10,7 @@ from docutils.parsers.rst import directives
 from typing import Any
 from sphinx.util.docutils import SphinxDirective
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 __display_version__ = __version__
 
 logger = logging.getLogger(__name__)
